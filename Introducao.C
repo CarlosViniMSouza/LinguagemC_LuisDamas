02d325528380a8a2e390c80ce6a316ef4d188579
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-04-12 22:30:25 -0300

    Introducao desenvolvida

---
+++ Introducao.C
@@ -0,0 +1,58 @@
+/*
+    Introdução
+
+Bem vindo ao mundo da programação em C !!
+
+Este livro tem como objetivo apresentar todas as características da Linguagem de
+Programação C. Além de dicas, truques e exercícios usados por DEV's sêniors.
+
+OBS.: Os exemplos aqui contidos, serão baseados no padrão ANSI C (para garantir o 
+seu funcionamento em qualquer arquitetura) !
+
+    Breve História da Linguagem C
+
+Criada em 1972 nos Bell Telephone Laboratories por Dennis Ritchie, para permitir
+a escrita de um S.O. (o Unix, precursor do Linux); usando recursos de alto nível,
+dessa forma, tornando desnecessário o uso do Assembly.
+
+Espalhou-se pelo mundo rapidamente, atráves da divulgação do Sistema Unix pelas
+universidades dos E.U.A.; ganhou muita popularidade, e o 1° livro escrito á respeito
+do C foi 'The C programming Language' ~ Kernighan & Ritchie
+
+A linguagem C proveio da linguagem B, criada nos Laboratórios Bell por Ken Thompson.
+
+    Mil e Uma Razões para Programar em C
+
+Conhecida por ser 'general purpose', esta linguagem se adequa a qualquer projeto, tais
+como S.O.'s; Interfaces gráficas; Processamento de Dados; mainframe's; etc. E além de
+tudo, é usada como linguagem-base de muitas outras L.P.'s (tais como Python, C++, Java,
+Ruby, PHP, etc)!
+
+Dentre suas principais características, destacam-se :
+
+    ° Rapidez --> Obtem performances parecidas com a do Assembly (usando instruções
+específicas - igualmente como COBOL e Pascal).
+    ° Simples --> Com uma sintaxe simples, palavaras-chaves reduzidas e um grupo seleto
+de dados básicos e de operadores, o que reduz o esforço preciso para usa-lo.
+    ° Portabilidade --> Pode ser compilado em qualquer máquina (isso graças ao padrão
+ANSI). O que reduz as chances do código enfrentar problemas de incompatibilidade.
+    ° Modular --> Facilita a separação de projetos por módulos diferentes independentes.
+Utilizando funções específicas  dentro de cada programa construído.
+    ° Alto Nível --> Permite maior acesso as funcionalidades do Assembly (assim como
+outras liguagens : BASIC, COBOL e Pascal). Fora isso, permite gerenciar o uso de memória
+por meio de endereços de qualquer variável e/ou função.
+    ° Bibliotecas --> Mutas vezes necessárias para a realização de diversas tarefas, por
+causa do fato da linguagem contér poucas 'palavras-chave', suas capacidades se tornam 
+muito limitadas (problema esse que pode ser contornado graças as inúmeras bibliotecas).
+    ° Macros --> Tal característica permite o aumento da velocidade de execução das
+aplicações sem aumentar a complexidade de escrita do código.
+    ° Foco --> Dá ao desenvolvedor a possibilidade de escrever o código de diversas
+formas e maneiras (seja juntadno tudo em uma única linha, seja dividindo o código do
+arquivo em várias linhas para melhor visualizar o arquivo).
+    ° Evolução --> É uma linguagem estável. Sua evolução fez surgir um novo conceito na
+programação : `Object Oriented Language` - o que deu origem ao C++ e ao Java.
+
+E outras razões erão encontradas no decorrer desse livro, espero que goste. ✌️🤓
+
+[Pgs : 1 ~ 3]
+*/
