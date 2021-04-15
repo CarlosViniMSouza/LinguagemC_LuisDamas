e429a0b76e84d73b1108f642db08ee9d9ea95db2
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-04-14 21:59:59 -0300

    Cap1 init

---
+++ Capitulo1/MeuPrimeiroProg.C
@@ -0,0 +1,31 @@
+/*
+    Na linguagem C, existe uma função para inserir todas as funções que
+serão utilizadas na execução do programa : main()
+
+    E lembrando que C é uma linguagem 'Case Sensitive', ela diferencia
+letras maiúsculas de minúsculas.
+
+    Conheça o mais famoso programa de todos : Hello World
+*/
+
+#include <stdio.h> //inclui funções de input e exit
+
+main() //inicialização do programa
+{ // o programa começa a receber instruções
+    printf("Hello World \n"); //a instrução de printar a frase 'Hello World'
+} // aqui o programa encerra o recebimento de instruções
+
+// Problema n1 : Crie um programa cuja saída seja :
+/*
+    C
+    is the greatest
+    Language
+*/
+
+// Solução :
+#include <stdio.h>
+
+main()
+{
+    printf("C \n is the greatest \n Language");
+}
