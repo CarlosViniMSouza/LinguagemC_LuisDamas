4dab00e2693ab48887307f48e22072e4c6aa9ba9
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-05-13 10:00:25 -0300

    Cap5 finish

--- Capitulo5/Funcoes_Parte3.C
+++ Capitulo5/Funcoes_Parte3.C
@@ -42,5 +42,43 @@ main(){
     NOTA (N1) : As variaveis declaradas dentro de uma função, só são conhecidas por aquela
 função (ou seja, tentar manipular essas variaveis em outra parte do código não é possível) !
 
+    Isso deve ocorrer dentro de qualquer função antes da intrução;
+    Ex.:
+
+    function(...){
+        <declarar-variaveis>
+
+        <instruções>
+    }
+
+    NOTA (N2) : Após a execução do programa, as variáveis locais são deletadas.
+
+    NOTA (N3) : Sempre tente recorrer as variaveis locais - para melhor gerenciamento
+    de sua aplicação.
+
+    Considerações Finais :
+
+    Bem, muitas das vezes, funções são mal elaboradas da Linguagem C, por conta do simples
+    fato de não haver um valor pré-estabelecido aos booleanos TRUE e FALSE. Veja o exemplo abaixo:
+
+    PROBLEMA RESOLVIDO (N1) : Crie um programa que verifique se 2 nums inteiros são equivalentes;
 
 */
+
+#include <stdio.h>
+
+int n1, n2;
+
+int compare(int n1, int n2){
+    if (n1 == n2){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+main(){
+    printf("Digite 2 valores inteiros: ");
+    scanf("%d %d", &n1, &n2);
+    compare(n1, n2);
+}
