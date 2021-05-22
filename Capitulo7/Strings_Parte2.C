2072b1688d4446016b865d1c90b2b67cef7df07b
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-05-22 13:09:23 -0300

    in progress...

--- Capitulo7/Strings_Parte2.C
+++ Capitulo7/Strings_Parte2.C
@@ -13,5 +13,14 @@
     3 --> char *strcpy(char *det, char *origem) --> Copia a 'string *origem' e cola no lugar da 'string *det'
 
     4 --> char *strcat(char *det, char *origem) --> Cola a 'string *origem' no final da 'string *det'
-    
+
+    5 --> char strcountc(char *s, char ch) --> Devolve quantos 'ch' estão contidas na string s
+
+    6 --> int strcountd(char *s) --> Devolve quantos dígitos existem na string s
+
+    7 --> int indchr(char *s, char ch) --> //Não entendi muito bem o que acontece !! Sorry
+
+    8 --> int strpal(char *s) --> Verifica se a string s é ou não Palindroma (retorn TRUE ou FALSE)
+
+    9 --> char *strrev(char *s) --> Inverte a string s e retorna-a invertida ao mesmo tempo
 */
