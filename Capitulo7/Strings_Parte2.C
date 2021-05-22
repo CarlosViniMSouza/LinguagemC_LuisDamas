99125e59342544f41ec42ed9ccc8b5d4d8d49762
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-05-22 12:45:27 -0300

    strings_parte2 in coming

--- Capitulo7/Strings_Parte2.C
+++ Capitulo7/Strings_Parte2.C
@@ -3,4 +3,15 @@
 
     Se dá pela mesma forma na passagem de vetores para funções, uma vez que qualquer
     string é sempre um vetor de caracteres;
+
+    Principais funções para maniipulação de Strings :
+
+    1 --> int strlen(char *s) --> Retorna o numero de caracateres existentes na string.
+
+    2 --> int isnull(char *s) --> Retorna TRUE(se for string vazia), Retorna FALSE(se for string não-vazia)
+
+    3 --> char *strcpy(char *det, char *origem) --> Copia a 'string *origem' e cola no lugar da 'string *det'
+
+    4 --> char *strcat(char *det, char *origem) --> Cola a 'string *origem' no final da 'string *det'
+    
 */
