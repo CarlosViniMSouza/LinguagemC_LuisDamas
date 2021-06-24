a003e8f0240f989f595dd35b348692628064ccbf
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-06-24 12:30:51 -0300

    Tarefa - cap1 add

---
+++ Capitulo1/Tarefa.C
@@ -0,0 +1,23 @@
+// Identifique os erros nos 3 programas abaixo:
+ 
+#include <stdio.h>
+
+// Numero1:
+Main(){
+
+    printf("Erro Num1 in Main()");
+    
+}
+
+//Numero2:
+main{
+
+    printf("Erro in main - againest");
+
+}
+
+main(){
+
+    print ("Now, the error is the command printf");
+
+}
