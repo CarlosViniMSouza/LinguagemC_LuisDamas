0cc551f26fc8aeed2f1c91b133a99e98a2e37cb9
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-06-25 17:07:23 -0300

    tarefa2 add

---
+++ Capitulo2/Tarefa.C
@@ -0,0 +1,25 @@
+// Aqui veremos algumas questões interessantes que possam fazer
+// valer o tempo que passamos estudando essa unidade.
+
+/*
+    Q1. Associe cada formato de dados a sua abreviação:
+
+    int --------------> %d
+    float ------------> %f
+    char -------------> %c
+    short int --------> %hd
+    long int ---------> %ld
+    signed long int --> %e
+
+    Q2. Identifique o erro(s) que seriam acusados no compilador:
+
+    #include <stdio.h>
+
+    main(){
+
+        int x, y, x;
+    
+    }
+    // erro na declaração da mesma variavel 2 vezes!
+    
+*/
